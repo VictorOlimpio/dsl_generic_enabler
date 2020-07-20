c733368ba6f75d0f9f4ff8b59b70dcc7c11e9a02
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2020-07-20 15:53:38 -0300

    configura o banco mysql

--- dslge/dslge/settings.py
+++ dslge/dslge/settings.py
@@ -74,11 +74,19 @@ WSGI_APPLICATION = 'dslge.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST':  '127.0.0.1',
+            'NAME':  'test',
+            'USER':  'root',
+            'PASSWORD':  'root',
+            'PORT':  '3306',
+            'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", },
+            'TEST': {
+                'NAME': 'test',
+            },
+        }
     }
-}
 
 
 # Password validation
