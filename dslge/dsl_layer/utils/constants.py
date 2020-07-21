969f83ef9069ba7703ba39fb079d6f2e04dfaa20
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2020-07-21 09:07:44 -0300

    adiciona pacote utils

---
+++ dslge/dsl_layer/utils/constants.py
@@ -0,0 +1,4 @@
+import os
+
+DOC_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__))))
---
+++ dslge/dsl_layer/utils/load_tables.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import io
+from dsl_layer.utils.constants import DOC_DIR
+
+def _file_serialize(filename, fieldnames=None, file_type='csv', sep=',', encoding='utf-8'):
+    df = pd.DataFrame()
+    if file_type == 'csv':
+        df = pd.read_csv(filename + '.csv', sep=sep,
+                         usecols=fieldnames, encoding=encoding)
+    else:
+        df = pd.read_excel(filename + '.' + file_type, usecols=fieldnames)
+    df.to_pickle(filename + '.pkl')
+    return df
+
+
+def _load_file(filename):
+    df = pd.DataFrame()
+    try:
+        df = pd.read_pickle(filename + '.pkl')
+        return df
+    except:
+        return df
+
+
+def load_serialized_file(path, fieldnames=None, file_type='csv', sep=',', encoding='utf-8'):
+    df = _load_file(path)
+    if df.empty:
+        return _file_serialize(path, fieldnames=fieldnames, file_type=file_type, sep=sep, encoding=encoding)
+    return df
+
+
+def load_df_from_table_with_col(filename, fieldname, nitems=0, fieldnames=None, file_type='csv', encoding='utf-8', sep=';'):
+    df = load_serialized_file(
+        DOC_DIR + filename, file_type=file_type, sep=sep, encoding=encoding, fieldnames=fieldnames)
+    df = df if nitems == 0 else df[0:nitems]
+    df.dropna(
+        axis=0, subset=[fieldname], inplace=True)
+    df.reset_index(inplace=True)
+    return df
+
+
+def load_df_from_table(filename, nitems=0, file_type='csv', encoding='utf-8', sep=';'):
+    df = load_serialized_file(
+        DOC_DIR + filename, file_type=file_type, sep=sep, encoding=encoding)
+    df = df if nitems == 0 else df[0:nitems]
+    return df
+
+
