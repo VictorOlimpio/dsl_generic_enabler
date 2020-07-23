dd2a7295fc967c6b2c8e858b57571dc2950e266d
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2020-07-23 10:22:36 -0300

    muda nome da tabela e adiciona migrações

--- dslge/dsl_layer/models.py
+++ dslge/dsl_layer/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Measures(models.Model):
+class Measure(models.Model):
     measure_one = models.FloatField(null = True, db_column = 'measure_one')
     measure_two = models.FloatField(null = True, db_column = 'measure_two')
     measure_three = models.FloatField(null = True, db_column = 'measure_three')
     measure_four = models.FloatField(null = True, db_column = 'measure_four')
     predicted_value = models.FloatField(null = True, db_column = 'predicted_value')
-    week_day = models.FloatField(null = True, db_column = 'week_day')
+    weekday = models.FloatField(null = True, db_column = 'weekday')
     weekend = models.FloatField(null = True, db_column = 'weekend')
+    season = models.FloatField(null=True, db_column= 'season')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
