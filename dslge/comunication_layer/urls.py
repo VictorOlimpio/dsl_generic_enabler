ef8b08ce7128efd78569f07812b36ba5fe0c1240
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2020-09-14 16:12:44 -0300

    feature: adiciona rota para health

---
+++ dslge/comunication_layer/urls.py
@@ -0,0 +1,15 @@
+from rest_framework import routers
+from comunication_layer.views import health
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path
+# router = routers.DefaultRouter()
+# router.register(r'health', IoTAgentHealthViewSet, basename='health')
+# urlpatterns = [
+#     url(r'health', include((router.urls, 'comunication_layer'))),
+# ]
+
+urlpatterns = [
+    path('health/', health),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
