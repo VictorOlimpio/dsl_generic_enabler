bb62ff437fbdce2062ef3a153d44f03d9e01c8a2
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2020-10-25 17:42:41 -0300

    feature: configure the models to work with mongodb properly

--- dslge/dsl_broker/models.py
+++ dslge/dsl_broker/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+class Model(models.Model):
+    modelName = models.CharField(max_length=100)
+    windowSize = models.CharField(max_length=10)
+    preTrainSize = models.CharField(max_length=10)
+    nSamples = models.CharField(max_length=10)
+    # model = models.FieldFile()
+    class Meta:
+        abstract = True
+
+class Subscription(models.Model):
+    url = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+class EntityModel(models.Model):
+    entity_id = models.CharField(max_length=100, primary_key=True, unique=True)
+    entity_type = models.CharField(max_length=100)
+    model = models.EmbeddedField(model_container=Model, default=None)
+    Subscription = models.EmbeddedField(model_container=Subscription, default=None)
+
